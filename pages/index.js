7ebf1fd004eca66e93ce33d3df9dbd9690da8166
Anish Karandikar <anishkny@gmail.com>
2021-07-30 17:53:10 -0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div>
+      Welcome to Next.js! <hr />
+      <span>The time is {new Date().toUTCString()}</span>
+    </div>
+  );
+}
+export default HomePage;
