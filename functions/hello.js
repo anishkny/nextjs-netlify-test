93f838cc013c53b7356d31277e48c42a5ea84bc2
Anish Karandikar <anishkny@gmail.com>
2021-07-31 07:20:13 -0700

    Log function calls

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,7 @@
 const generateRandomNumber = require('./util/generateRandomNumber');
 
 exports.handler = async function (event, context) {
+  console.log(`${event.httpMethod} ${event.path}`);
   return {
     statusCode: 200,
     body: JSON.stringify(
